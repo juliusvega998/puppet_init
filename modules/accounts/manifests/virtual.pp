a8e3ca2c4bce6b949ac721f7c24cb7658068bcda
juliusvega998 <juliusvega998@gmail.com>
2017-06-01 01:44:51 -0400

    Added timezone setting

--- modules/accounts/manifests/virtual.pp
+++ modules/accounts/manifests/virtual.pp
@@ -1,9 +1,14 @@
 define accounts::virtual ($uid, $realname, $pass, $email) {
-
+	
+	# install vim, curl, and git
 	package { 'vim-minimal':  ensure => 'installed' }
 	package { 'curl': ensure => 'installed' }
 	package { 'git':  ensure => 'installed' }
 
+	# needed for memory checker	
+	package { 'bc':	  ensure => 'installed' }
+
+	# create user based from the blog
 	user { $title:
 		ensure		=> 'present',
 		uid		=> $uid,
@@ -28,6 +33,7 @@ define accounts::virtual ($uid, $realname, $pass, $email) {
 		require		=> [ User[$title], Group[$title] ],
 	}
 
+	# create folders
 	file { "/home/${title}/scripts":
 		ensure		=> directory,
 	}
@@ -36,6 +42,7 @@ define accounts::virtual ($uid, $realname, $pass, $email) {
 		ensure		=> directory,
 	}
 
+	# get the memory check script and mvoe it to scripts folder
 	exec { 'get_script':
 		command		=> 'wget https://raw.githubusercontent.com/juliusvega998/centos_memory_checker/master/memory_check.sh',
 		path 		=> '/usr/bin',
@@ -51,16 +58,23 @@ define accounts::virtual ($uid, $realname, $pass, $email) {
 		path		=> '/bin',
 	}
 
+	# create a soft link of the script
 	exec { 'soft_link_script':
 		command		=> "ln -s /home/${title}/scripts/memory_check.sh /home/${title}/src/my_memory_check",
 		path		=> '/bin',
 	}
-
+	
+	#create a crontab entry to run the script every 10 mins
 	cron { 'memory_check':
 		command		=> "./home/${titlle}/src/my_memory_check -c 90 -w 60 -e ${email}",
 		user		=> $title,
 		hour		=> 0,
 		minute		=> 10,
+	}
+
+	# set timezone to PHT
+	zone { 'timezone':
+		sysidcfg	=> template('accounts/sysidcfg.erb'),
 	}
 }
 
